c04d753acf166fd93216d2eff45faea5b1850afb
ivan-boginski <aisivan36@gmail.com>
2021-10-11 21:42:30 +0700

    Managed splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../models/models.dart';
 
 class SplashScreen extends StatefulWidget {
-  // TODO: SplashScreen MaterialPage Helper
+  static MaterialPage page() {
+    return MaterialPage(
+      name: FooderlichPages.splashPath,
+      key: ValueKey(FooderlichPages.splashPath),
+      child: const SplashScreen(),
+    );
+  }
 
   const SplashScreen({Key? key}) : super(key: key);
 
@@ -13,7 +21,10 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-    // TODO: Initialize App
+
+    /// Here we use the current [context] to retrieve the [AppStateManager]
+    /// to initialize the app
+    Provider.of<AppStateManager>(context, listen: false).initializeApp();
   }
 
   @override
